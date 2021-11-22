cc19b39165773b556b30dd426b80a32353cf2760
AbrahamJR1 <89867461+AbrahamJR1@users.noreply.github.com>
2021-11-22 12:39:57 -0600

    Update claculadora.c

--- claculadora.c
+++ claculadora.c
@@ -11,70 +11,70 @@ int main()
 
     while ( n3 != 0){
 
-    printf("\nPrecione 1 si quiere hacer una suma ");
+    printf("\nPresione 1 si quiere hacer una suma ");
     printf("\n--------------------------------------");
-    printf("\nPrecione 2 si quiere hacer una multiplicacion");
+    printf("\nPresione 2 si quiere hacer una multiplicacion");
     printf("\n----------------------------------------------");
-    printf("\nPrecione 3 si quiere hacer una divicion");
+    printf("\nPresione 3 si quiere hacer una divicion");
     printf("\n-----------------------------------------");
-    printf("\nPrecione 4 si quiere hacer una resta");
+    printf("\nPresione 4 si quiere hacer una resta");
     printf("\n--------------------------------------");
-    printf("\nPrecione 5 si quiere convertir pesos a dolares");
+    printf("\nPresione 5 si quiere convertir pesos a dolares");
     printf("\n--------------------------------------");
-    printf("\nPrecione 0 si salir");
+    printf("\nPresione 0 si salir");
     printf("\n-----------------------");
     printf("\n");
     scanf("%f",&n3);
     
     
     if ( n3 == 1) {
-    printf("dame el primer dijito: ");
+    printf("Dame el primer dijito: ");
     scanf("%f" ,&n1);
-    printf("dame el segundo dijito: ");
+    printf("Dame el segundo dijito: ");
     scanf("%f" ,&n2);
     
     suma = n1 + n2;
     
-    printf ("\n la suma es %5.2f" ,suma);
+    printf ("\n La suma es %5.2f" ,suma);
     printf("\n");
     }
 
     if ( n3 == 2) {
-    printf("dame el primer dijito: ");
+    printf("Dame el primer dijito: ");
     scanf("%f" ,&n1);
-    printf("dame el segundo dijito: ");
+    printf("Dame el segundo dijito: ");
     scanf("%f" ,&n2);
     
     multi = n1 * n2;
     
-    printf ("\n la multiplicacion es %5.2f" ,multi);
+    printf ("\n La multiplicacion es %5.2f" ,multi);
     printf("\n");
     }
 
 
     if ( n3 == 3) {
-    printf("dame el primer dijito: ");
+    printf("Dame el primer dijito: ");
     scanf("%f" ,&n1);
-    printf("dame el segundo dijito: ");
+    printf("Dame el segundo dijito: ");
     scanf("%f" ,&n2);
     
     
     divicion = n1 / n2;
     
-    printf ("\n la divicion es %5.2f" ,divicion);
+    printf ("\n La divicion es %5.2f" ,divicion);
     printf("\n");
     }
 
 
     if ( n3 == 4) {
-    printf("dame el primer dijito: ");
+    printf("Dame el primer dijito: ");
     scanf("%f" ,&n1);
-    printf("dame el segundo dijito: ");
+    printf("Dame el segundo dijito: ");
     scanf("%f" ,&n2);
     
     resta = n1 - n2;
     
-    printf ("\n la resta es %5.2f" ,resta);
+    printf ("\n La resta es %5.2f" ,resta);
     printf("\n");
     }
 
